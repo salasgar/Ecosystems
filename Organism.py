c8b3af7d0d57ff43812090334a80572d6a108831
salasgar <JuanLuis@JLSalas-Portatil.wifi.educarm.net>
2015-04-13 10:03:36 +0200

    Organism with bugs

---
+++ Organism.py
@@ -0,0 +1,70 @@
+
+def coordinatesDict(coordTuple):
+    return {'x': coordTuple[0], 'y': coordTuple[1]}
+    
+def coordinatesTuple(coordDict):
+    return (coordDict['x'], coordDict['y'])
+
+class Organism:
+    Data = None  # All organism's attributes (genes and status)
+    
+    def __init__(self, Data):
+        self.Data = Data
+    
+    def __getitem__(self, keys):
+        return self.Data[keys]
+        
+    def __setitem__(self, keys, value):
+        self.Data[keys] = value        
+        
+    def genes(self):
+        return self.Data['genes']
+        
+    def status(self):
+        return self.Data['status']
+        
+    def position(self):
+        return coorinatesTuple(self.Data['status']['coordinates'])     
+    
+    def speed(self):
+        return self.Data['genes']['speed']
+        
+    def move(self, ecosystem):
+        if self.speed() > 0:
+            new_place = ecosystem.biotope.seek_free_pos_close_to(self.position(), self.speed(), 3)
+            if new_place != None:
+                ecosystem.biotope.move_org(self, new_place)
+                self.Data['status']['coordinates'] = coordinatesDict(new_place)
+        pass
+
+    def eat(self, organism, ecosystem):
+        pass
+
+    def do_photosynthesis(self, ecosystem):
+        pass
+
+    def procreate(self, ecosystem):
+        # TEMPORARY EXAMPLE
+        # add new organisms at the beginning of the list (as a queue)
+        # using ecosystem.organisms.insert(0, new_organism)
+        from random import random
+        if random() > 0.99:
+            print ('Oh organism %d-th had a baby:' %
+                   ecosystem.organisms.index(organism))
+            ecosystem.organisms.insert(0, organism.copy())  # Mutation?
+            print 'Num of organisms: %d' % len(ecosystem.organisms)
+            return 1
+        else:
+            return 0
+
+    def check_if_die_and_delete(self, ecosystem):
+        # Temporary random delete
+        from random import random
+        if random() > 0.98:
+            print ('%d-th organism DIED:' %
+                   ecosystem.organisms.index(organism))
+            ecosystem.organisms.remove(organism)
+            print 'Num of organisms: %d' % len(ecosystem.organisms)
+            return 1
+        else:
+            return 0
