e6045df35897534f1b0c608121db5a149ebb423b
Emilio Molina <emilio.mol.mar@gmail.com>
2020-05-31 17:52:22 +0200

    Update python bindings

--- pruebas_cpp/ecosystem_bindings.cpp
+++ pruebas_cpp/ecosystem_bindings.cpp
@@ -1,12 +1,23 @@
 #include <pybind11/pybind11.h>
-#include "ecosystem.h"
+#include "classes.hpp"
 
 namespace py = pybind11;
 
+
+
 PYBIND11_MODULE(eco, m) {
+  py::enum_<OrganismType>(m, "OrganismType")
+    .value("PLANT_A", OrganismType::PLANT_A)
+    .value("PLANT_B", OrganismType::PLANT_B)
+    .value("HERBIVORE", OrganismType::HERBIVORE)
+    .value("CARNIVORE", OrganismType::CARNIVORE)
+    .export_values();
+
   py::class_<Ecosystem>(m, "Ecosystem")
     .def(py::init<>())
+    .def("initialize", &Ecosystem::initialize)
     .def("get_num_organisms", &Ecosystem::get_num_organisms)
+    .def("create_new_organisms", &Ecosystem::create_new_organisms)
     .def("evolve", &Ecosystem::evolve);
 }
 
--- pruebas_cpp/test_eco.py
+++ pruebas_cpp/test_eco.py
@@ -3,11 +3,17 @@ import eco
 
 start = time.time()
 e = eco.Ecosystem()
+e.initialize()
+e.create_new_organisms(eco.OrganismType.PLANT_A, 5000);
+e.create_new_organisms(eco.OrganismType.PLANT_B, 5000);
+e.create_new_organisms(eco.OrganismType.HERBIVORE, 5000);
+e.create_new_organisms(eco.OrganismType.CARNIVORE, 5000);
 print("init time: {:.2f}ms".format(1000 * (time.time() - start)))
-start = time.time()
 num_iters = 1000
 for i in range(num_iters):
+    start = time.time()
+    print("cycle: {0}".format(i))
     e.evolve()
-print(e.get_num_organisms())
-iteration_time_ms = 1000 * (time.time() - start) / num_iters
-print("avg iteration time: {:.2f}ms".format(iteration_time_ms))
+    print("num_organisms: {0}".format(e.get_num_organisms()))
+    iteration_time_ms = 1000 * (time.time() - start)
+    print("iteration time: {:.2f}ms".format(iteration_time_ms))
